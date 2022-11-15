69fa80ceb7301d734630f29dbfe1cac29be32f24
C04t0 <oscar.yde@gmail.com>
2022-11-15 11:12:55 +0100

    commit 2

---
+++ src/test/AreaHex.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Scanner;
+
+public class AreaHex {
+    public static void areaHex() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Give a int for side: ");
+        int s = keyboard.nextInt();
+        double areaHex = (6*Math.pow(s, 2.0))/(4*Math.tan(Math.PI/6));
+        System.out.println(areaHex);
+        keyboard.close();
+    }
+}
+
